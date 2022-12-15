741031bfdc39e21a01c5c3ce5ea1e65433dcaecb
filippabostrom <bostromfilippa@gmail.com>
2022-12-15 15:29:39 +0100

    databas inlagd

--- src/mib/MIB.java
+++ src/mib/MIB.java
@@ -3,18 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package mib;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import oru.inf.InfDB;
+import oru.inf.InfException;
 
-/**
- *
- * @author Idawi
- */
 public class MIB {
+    private static InfDB idb;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) throws InfException {
+       try {
+           idb = new InfDB ("mibdb","3306","mibdba","mibkey");
+       } catch (InfException ex) {
+           Logger.getLogger(MIB.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       
+    }
 }
+
